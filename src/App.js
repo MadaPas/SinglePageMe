cdef6b3c6f9c313c43e5adbb66b9fdeb4b5f1fa9
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-09 18:03:26 +0100

    outline of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./styles/name.scss";
+import { Footer } from "./components/Footer";
+import { Content } from "./components/Content";
 
-export default App;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+    },
+    main: {
+        marginTop: theme.spacing(8),
+        marginBottom: theme.spacing(2),
+    },
+    footer: {
+        padding: theme.spacing(3, 2),
+        marginTop: "auto",
+        backgroundColor:
+            theme.palette.type === "light"
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+    },
+    fab: {
+        margin: theme.spacing(2),
+    },
+}));
+
+export const App = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Content mainClasses={classes.main} />
+            <Footer footerClasses={classes.footer} />
+        </div>
+    );
+};
