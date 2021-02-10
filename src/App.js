9b74d5b39518a6e3a94316289a1ef9205e5bc242
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-10 20:21:04 +0100

    cleanup after crowling in App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import "./styles/name.scss";
 import { ThemeProvider } from "./components/Theme/ThemeProvider";
 import { Credits } from "./utils/Credits";
 import { Home } from "./pages/Home";
-// import { Loading } from "./components/Loading/Loading";
-// import { LoadingError } from "./components/Loading/LoadingError";
-// import { ErrorBoundary } from "./components/Loading/ErrorBoundary";
+
 const Resume = lazy(() => import("./pages/Resume"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
@@ -21,15 +19,6 @@ export const App = () => {
         <ThemeProvider>
             <CssBaseline />
             <Router>
-                {/* <ErrorBoundary fallback={<LoadingError />}>
-                    <Suspense fallback={<Loading />}>
-                        <Switch>
-                            <Route path="/" exact component={Home} />
-                            <Route path="/resume" component={Resume} />
-                            <Route path="*" component={NotFound} />
-                        </Switch>
-                    </Suspense>
-                </ErrorBoundary> */}
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/resume" component={Resume} />
